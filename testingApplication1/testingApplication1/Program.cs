980f40fa6f3b54b17e591932f4c824876b5aae75
Tiia Itkonen <K1982@student.jamk.fi>
2016-01-20 08:53:04 +0200

    added readline

--- testingApplication1/testingApplication1/Program.cs
+++ testingApplication1/testingApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace testingApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub!");
+            Console.ReadLine();
         }
     }
 }
