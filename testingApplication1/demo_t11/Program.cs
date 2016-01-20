be3c619ff8ef5ced0ef40edede012717b5093b02
Tiia Itkonen <K1982@student.jamk.fi>
2016-01-20 11:08:23 +0200

    kesken

---
+++ testingApplication1/demo_t11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_t11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rivinro, sarakenro;
+
+            Console.WriteLine("Anna luku \n");
+            string line = Console.ReadLine();
+            int number = int.Parse(line);
+
+            for (int = i; rivinro <= rivilkm; rivinro++) //KESKEN!!!
+            {
+                for (sarakenro = 1; sarakenro <= rivinro; sarakenro++)
+                {
+                    Console.WriteLine(rivinro);
+                }
+
+            }
+        }
+    }
+}
+
