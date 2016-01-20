07c27ea31b980499f70a5df7318eacb04668696d
Tiia Itkonen <K1982@student.jamk.fi>
2016-01-20 10:18:02 +0200

    new project

---
+++ testingApplication1/demo1_teht5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1_teht5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Give seconds: ");
+            string line = Console.ReadLine();
+            int seconds = int.Parse(line);
+
+            int hour = seconds / 3600;
+            int min = seconds / 60 - hour * 60;
+            int second = seconds & 60;
+
+            Console.WriteLine("Time: {0}h {1}m {2}s", hour, min, second);
+            Console.ReadLine();
+
+        }
+    }
+}
